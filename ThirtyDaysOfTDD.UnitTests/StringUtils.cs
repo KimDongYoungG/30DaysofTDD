e2f2a2416d69d107cff9bb7d21a34621b81251c9
DESKTOP-8N15U0C\DongyoungKim <dykimblue@gmail.com>
2018-12-18 13:45:59 +0900

    <initial> init project

---
+++ ThirtyDaysOfTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysOfTDD.UnitTests
+{
+    class StringUtils
+    {
+        internal int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            try
+            {
+                var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+                var characterToCheckFor = Char.Parse(characterToScanFor);
+                var numberOfOccurenes = 0;
+
+                for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
+                {
+                    if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
+                    {
+                        numberOfOccurenes++;
+                    }
+                }
+
+                return numberOfOccurenes;
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+
+            
+        }
+    }
+}
